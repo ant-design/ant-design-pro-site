826fc274649a727b13a2b4a6afd43799117e7faf
nikogu <644506165@qq.com>
2017-07-20 15:47:19 +0800

    删除注释错的代码

--- tool/lib/move.js
+++ tool/lib/move.js
@@ -13,18 +13,18 @@ module.exports = function (layout) {
   const siteDir = path.join(cwd, './_site');
 
   // change name
-  //try {
-    //fs.moveSync(path.join(scaffoldDir, './index.css'), path.join(scaffoldDir, './s.css'));
-    //fs.moveSync(path.join(scaffoldDir, './index.js'), path.join(scaffoldDir, './s.js'));
-    //fs.moveSync(path.join(scaffoldDir, './index.html'), path.join(scaffoldDir, './s.html'));
-  //} catch(e) {
-  //  throw new Error(e);
-  //}
+  try {
+    fs.moveSync(path.join(scaffoldDir, './index.css'), path.join(scaffoldDir, './s.css'));
+    fs.moveSync(path.join(scaffoldDir, './index.js'), path.join(scaffoldDir, './s.js'));
+    fs.moveSync(path.join(scaffoldDir, './index.html'), path.join(scaffoldDir, './s.html'));
+  } catch (e) {
+    throw new Error(e);
+  }
 
   // move
   try {
     fs.copySync(scaffoldDir, siteDir);
-  } catch(e) {
+  } catch (e) {
     throw new Error(e);
   }
 
