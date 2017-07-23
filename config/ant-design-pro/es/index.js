c6e750d603b86a02cefa2391d2bf598e206f6755
nikogu <644506165@qq.com>
2017-07-23 12:53:34 +0800

    删除无用文件

--- config/ant-design-pro/es/index.js
+++
@@ -1,9 +0,0 @@
-import HeaderSearch from '../../scaffold/src/components/HeaderSearch';
-import NotificationIcon from '../../scaffold/src/components/NotificationIcon';
-import PageHeader from '../../scaffold/src/components/PageHeader';
-
-export default {
-  HeaderSearch,
-  NotificationIcon,
-  PageHeader,
-};
