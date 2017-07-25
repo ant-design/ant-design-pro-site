d9bf0ce98e20332d98a96be25036f7f21193d465
nikogu <644506165@qq.com>
2017-07-25 18:27:23 +0800

    standardTable 框架搭建

--- scaffold/src/components/StandardTable/index.js
+++ scaffold/src/components/StandardTable/index.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Table, Alert } from 'antd';
+import { Table, Alert, Badge } from 'antd';
 import styles from './index.less';
+
+const dataSource = [];
+for (let i = 0; i < 46; i += 1) {
+  dataSource.push({
+    key: i,
+    no: `TradeCode ${i}`,
+    description: '这是一段描述',
+    callNo: Math.floor(Math.random() * 1000),
+    status: Math.floor(Math.random() * 10) % 2,
+    updatedAt: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+  });
+}
 
 class StandardTable extends Component {
   state = {
@@ -11,33 +23,24 @@ class StandardTable extends Component {
     loading: false,
   };
 
-  onRowSelectChange = (selectedRowKeys, selectedRows) => {
+  handleRowSelectChange = (selectedRowKeys, selectedRows) => {
     this.setState({ selectedRowKeys, selectedRows });
   }
 
-  onPageChange = (currentPage) => {
+  handlePageChange = (currentPage) => {
     this.setState({
       currentPage,
     });
   }
 
+  handleTableChange = (pagination, filters, sorter) => {
+    console.log(pagination, filters, sorter);
+  }
+
   render() {
     const { selectedRowKeys, currentPage } = this.state;
 
-
     const status = ['关闭', '运行中'];
-
-    const dataSource = [];
-    for (let i = 0; i < 46; i += 1) {
-      dataSource.push({
-        key: i,
-        no: `TradeCode ${i}`,
-        description: '这是一段描述',
-        callNo: Math.floor(Math.random() * 1000),
-        status: Math.floor(Math.random() * 10) % 2,
-        updatedAt: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-      });
-    }
 
     const columns = [
       {
@@ -51,6 +54,7 @@ class StandardTable extends Component {
       {
         title: '服务调用次数',
         dataIndex: 'callNo',
+        sorter: true,
       },
       {
         title: '状态',
@@ -62,13 +66,21 @@ class StandardTable extends Component {
           },
           {
             text: status[1],
-            value: 0,
+            value: 1,
           },
         ],
+        render(val) {
+          if (val === 0) {
+            return <Badge status="default" text={status[val]} />;
+          } else {
+            return <Badge status="processing" text={status[val]} />;
+          }
+        },
       },
       {
         title: '更新时间',
         dataIndex: 'updatedAt',
+        sorter: true,
       },
       {
         title: '操作',
@@ -88,12 +100,12 @@ class StandardTable extends Component {
       pageSize: 10,
       total: 111,
       current: currentPage,
-      onChange: this.onPageChange,
+      onChange: this.handlePageChange,
     };
 
     const rowSelection = {
       selectedRowKeys,
-      onChange: this.onRowSelectChange,
+      onChange: this.handleRowSelectChange,
     };
 
     return (
@@ -103,10 +115,12 @@ class StandardTable extends Component {
         </div>
 
         <Table
+          rowKey={record => record.key}
           rowSelection={rowSelection}
           dataSource={dataSource}
           columns={columns}
           pagination={pagination}
+          onChange={this.handleTableChange}
         />
       </div>
     );
