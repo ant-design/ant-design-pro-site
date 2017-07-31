0cf526f86c893570e526e2e105f2d0ec9b3da6db
ddcat1115 <ddcat1115@gmail.com>
2017-08-01 00:41:43 +0800

    Result (#30)
    * Result
    
    * fix lint
    
    * use less variable

--- config/components/index.js
+++ config/components/index.js
@@ -1,9 +1,11 @@
 import HeaderSearch from '../../scaffold/src/components/HeaderSearch';
 import NotificationIcon from '../../scaffold/src/components/NotificationIcon';
 import PageHeader from '../../scaffold/src/components/PageHeader';
+import Result from '../../scaffold/src/components/Result';
 
 export default {
   HeaderSearch,
   NotificationIcon,
   PageHeader,
+  Result,
 };
