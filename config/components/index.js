4aab4bfc50d3af663b174e5dbf004442a65e6641
乐仪 <linya.qu@alipay.com>
2017-07-31 17:37:34 +0800

    Exception

--- config/components/index.js
+++ config/components/index.js
@@ -1,9 +1,11 @@
 import HeaderSearch from '../../scaffold/src/components/HeaderSearch';
 import NotificationIcon from '../../scaffold/src/components/NotificationIcon';
 import PageHeader from '../../scaffold/src/components/PageHeader';
+import Exception from '../../scaffold/src/components/Exception';
 
 export default {
   HeaderSearch,
   NotificationIcon,
   PageHeader,
+  Exception,
 };
