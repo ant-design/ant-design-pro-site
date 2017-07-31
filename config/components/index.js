45362050aa6d52b9024d76580f589f044c98d0c7
ddcat1115 <ddcat1115@gmail.com>
2017-08-01 00:56:29 +0800

    GlobalFooter (#29)
    * GlobalFooter
    
    * fix indent & add `blankTarget`
    
    * 	data 属性拆分为 links & copyright

--- config/components/index.js
+++ config/components/index.js
@@ -1,11 +1,13 @@
 import HeaderSearch from '../../scaffold/src/components/HeaderSearch';
 import NotificationIcon from '../../scaffold/src/components/NotificationIcon';
 import PageHeader from '../../scaffold/src/components/PageHeader';
+import GlobalFooter from '../../scaffold/src/components/GlobalFooter';
 import Result from '../../scaffold/src/components/Result';
 
 export default {
   HeaderSearch,
   NotificationIcon,
   PageHeader,
+  GlobalFooter,
   Result,
 };
