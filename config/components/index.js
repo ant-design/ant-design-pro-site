4d39e1cc957d1ae3c7c6143df1b28be68ad0e914
ddcat1115 <ddcat1115@gmail.com>
2017-08-01 04:01:08 +0800

    Exception (#35)
    * Exception
    
    * use less variable

--- config/components/index.js
+++ config/components/index.js
@@ -1,6 +1,7 @@
 import HeaderSearch from '../../scaffold/src/components/HeaderSearch';
 import NotificationIcon from '../../scaffold/src/components/NotificationIcon';
 import PageHeader from '../../scaffold/src/components/PageHeader';
+import Exception from '../../scaffold/src/components/Exception';
 import GlobalFooter from '../../scaffold/src/components/GlobalFooter';
 import Result from '../../scaffold/src/components/Result';
 
@@ -8,6 +9,7 @@ export default {
   HeaderSearch,
   NotificationIcon,
   PageHeader,
+  Exception,
   GlobalFooter,
   Result,
 };
