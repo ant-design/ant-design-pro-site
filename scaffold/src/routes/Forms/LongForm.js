2b90813bfb45b03c9dfcb04c97ea650f645b42cf
afc163 <afc163@gmail.com>
2017-08-08 21:43:34 +0800

    Add some content into log form page

--- scaffold/src/routes/Forms/LongForm.js
+++ scaffold/src/routes/Forms/LongForm.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { Card, Button } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import FooterToolbar from '../../components/FooterToolbar';
+import MultipleColForm from './MultipleColForm';
+import styles from './style.less';
 
 function LongForm() {
   return (
-    <PageHeaderLayout title="长表单" content="在后台页面中，大批量的数据修改和提交是很常见的情况。">
-      <Card bordered={false}>
-        111
+    <PageHeaderLayout
+      title="长表单"
+      content="在后台页面中，大批量的数据修改和提交是很常见的情况。"
+    >
+      <Card title="分组表单" className={styles.card} bordered={false}>
+        <MultipleColForm />
+      </Card>
+      <Card title="成员管理" className={styles.card} bordered={false}>
+        ...
+      </Card>
+      <Card title="审批员管理" className={styles.card} bordered={false} style={{ marginBottom: 84 }}>
+        ...
       </Card>
       <FooterToolbar>
         <Button>取消</Button>
