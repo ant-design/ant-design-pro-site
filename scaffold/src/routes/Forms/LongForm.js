6b5fc09f125d40bd4dd9d4de9b55bb7922d9af86
偏右 <afc163@gmail.com>
2017-08-08 21:25:38 +0800

    Add FooterToolbar (#41)

--- scaffold/src/routes/Forms/LongForm.js
+++ scaffold/src/routes/Forms/LongForm.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { connect } from 'dva';
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import FooterToolbar from '../../components/FooterToolbar';
 
 function LongForm() {
   return (
-    <PageHeaderLayout title="长表单">
+    <PageHeaderLayout title="长表单" content="在后台页面中，大批量的数据修改和提交是很常见的情况。">
       <Card bordered={false}>
-        ...
+        111
       </Card>
+      <FooterToolbar>
+        <Button>取消</Button>
+        <Button type="primary">提交</Button>
+      </FooterToolbar>
     </PageHeaderLayout>
   );
 }
 
-export default connect()(LongForm);
+export default LongForm;
