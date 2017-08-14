83d6e3bb52fe7f3764661fc013dbb3d7f4d19774
afc163 <afc163@gmail.com>
2017-08-14 15:12:13 +0800

    Remove js-beauty

--- tool/lib/copy.js
+++ tool/lib/copy.js
@@ -1,16 +1,10 @@
 const fs = require('fs-extra');
-const beautify = require('js-beautify').js_beautify;
 
 const cwd = process.cwd();
 
 module.exports = function () {
   const pkg = JSON.parse(fs.readFileSync(`${cwd}/scaffold/package.json`, 'utf8'));
   const antdProPkg = JSON.parse(fs.readFileSync(`${cwd}/config/components/package.json`, 'utf8'));
-
   antdProPkg.dependencies = pkg.dependencies;
-
-  const newAntdProPkg = beautify(JSON.stringify(antdProPkg), { indent_size: 2 });
-
-  fs.writeFileSync(`${cwd}/config/components/package.json`, newAntdProPkg, 'utf8');
+  fs.writeFileSync(`${cwd}/config/components/package.json`, JSON.stringify(antdProPkg, null, 2), 'utf8');
 };
-
