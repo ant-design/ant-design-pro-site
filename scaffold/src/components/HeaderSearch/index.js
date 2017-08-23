1f6d78fb2ccdcd5508e6056f85734936b2307c94
偏右 <afc163@gmail.com>
2017-08-23 21:15:47 +0800

    Improve header search (#62)

--- scaffold/src/components/HeaderSearch/index.js
+++ scaffold/src/components/HeaderSearch/index.js
@@ -1,12 +1,29 @@
 import React, { PureComponent } from 'react';
-import { Input, Icon } from 'antd';
+import { Input, Icon, AutoComplete } from 'antd';
 import classNames from 'classnames';
 import styles from './index.less';
 
 export default class HeaderSearch extends PureComponent {
+  static defaultProps = {
+    defaultActiveFirstOption: false,
+  };
   state = {
     searchMode: false,
+    value: '',
   };
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+  onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.timeout = setTimeout(() => {
+        this.props.onPressEnter(this.state.value);  // Fix duplicate onPressEnter
+      }, 0);
+    }
+  }
+  onChange = (value) => {
+    this.setState({ value });
+  }
   enterSearchMode = () => {
     this.setState({ searchMode: true }, () => {
       if (this.state.searchMode) {
@@ -15,22 +32,33 @@ export default class HeaderSearch extends PureComponent {
     });
   }
   leaveSearchMode = () => {
-    this.setState({ searchMode: false });
+    this.setState({
+      searchMode: false,
+      value: '',
+    });
   }
   render() {
-    const { className, placeholder } = this.props;
+    const { className, placeholder, ...restProps } = this.props;
     const inputClass = classNames(styles.input, {
       [styles.show]: this.state.searchMode,
     });
     return (
       <span className={className} onClick={this.enterSearchMode}>
         <Icon type="search" />
-        <Input
+        <AutoComplete
           className={inputClass}
-          placeholder={placeholder}
-          ref={(node) => { this.input = node; }}
-          onBlur={this.leaveSearchMode}
-        />
+          value={this.state.value}
+          onChange={this.onChange}
+          onSelect={this.onSelect}
+          {...restProps}
+        >
+          <Input
+            placeholder={placeholder}
+            ref={(node) => { this.input = node; }}
+            onKeyDown={this.onKeyDown}
+            onBlur={this.leaveSearchMode}
+          />
+        </AutoComplete>
       </span>
     );
   }
