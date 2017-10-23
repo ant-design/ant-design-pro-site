edfce405c28f34e4927d6865958fb8ad6e61664e
nikogu <644506165@qq.com>
2017-10-23 18:06:28 +0800

    update  submodule

--- site/theme/template/Content/index.jsx
+++ site/theme/template/Content/index.jsx
@@ -24,7 +24,7 @@ export default collect(async (nextProps) => {
 
   // 路由跳转统一处理
   if (pathname === 'components') {
-    location.href = '/components/PageHeader';
+    location.href = '/components/AvatarList';
     return;
   }
 
