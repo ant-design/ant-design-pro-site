8142a896b11b4e081a4b35ec09e9306175fce98a
afc163 <afc163@gmail.com>
2017-10-27 23:02:42 +0800

    Fix g2 config

--- site/theme/template/Layout/index.jsx
+++ site/theme/template/Layout/index.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
+import G2 from 'g2';
 
 import enLocale from '../../en-US';
 import cnLocale from '../../zh-CN';
@@ -11,6 +12,31 @@ import * as utils from '../utils';
 
 import Header from './Header';
 import Footer from './Footer';
+
+// 全局 G2 设置
+G2.track(false);
+
+const colors = [
+  '#8543E0', '#F04864', '#FACC14', '#1890FF', '#13C2C2', '#2FC25B', '#fa8c16', '#a0d911',
+];
+
+const config = {
+  ...G2.Theme,
+  defaultColor: '#1089ff',
+  colors: {
+    default: colors,
+    intervalStack: colors,
+  },
+  tooltip: {
+    background: {
+      radius: 4,
+      fill: '#000',
+      fillOpacity: 0.75,
+    },
+  },
+};
+
+G2.Global.setTheme(config);
 
 if (typeof window !== 'undefined') {
   /* eslint-disable global-require */
