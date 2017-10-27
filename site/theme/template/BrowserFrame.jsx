2c2171710163b87af1e1cbd074d591325758ab47
nikogu <644506165@qq.com>
2017-10-27 18:04:14 +0800

    fixed iframe demo

--- site/theme/template/BrowserFrame.jsx
+++ site/theme/template/BrowserFrame.jsx
@@ -12,9 +12,9 @@ export default class BrowserFrame extends React.Component {
         iframe.onload = () => {
           const h = iframe.contentWindow.document.head;
           const links = h.getElementsByTagName('link');
-          links[0].setAttribute('href', `${(!isDev && root) || '..'}/index-1.css`);
-          links[1].setAttribute('href', `${(!isDev && root) || '..'}/index-2.css`);
-          links[2].setAttribute('href', `${(!isDev && root) || '..'}/index-3.css`);
+          links[0].setAttribute('href', `${(!isDev && root) || '../'}index-1.css`);
+          links[1].setAttribute('href', `${(!isDev && root) || '../'}index-2.css`);
+          links[2].setAttribute('href', `${(!isDev && root) || '../'}index-3.css`);
           links[2].onload = () => {
             iframe.contentWindow.document.body.style.opacity = 1;
           };
