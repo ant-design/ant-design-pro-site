22bd92cdf6dc40cdc0a123a8046eb02160190b03
nikogu <644506165@qq.com>
2017-11-01 15:31:24 +0800

    remove lerna && auto set pro version

--- tool/lib/copy.js
+++ tool/lib/copy.js
@@ -5,6 +5,7 @@ const cwd = process.cwd();
 module.exports = function () {
   const pkg = JSON.parse(fs.readFileSync(`${cwd}/scaffold/package.json`, 'utf8'));
   const antdProPkg = JSON.parse(fs.readFileSync(`${cwd}/config/components/package.json`, 'utf8'));
+  antdProPkg.version = pkg.version;
   antdProPkg.dependencies = pkg.dependencies;
   fs.writeFileSync(`${cwd}/config/components/package.json`, JSON.stringify(antdProPkg, null, 2), 'utf8');
 };
