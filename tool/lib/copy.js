307b64b92f6bf40951b0c48dc03866223d0fecbf
nikogu <644506165@qq.com>
2017-11-02 13:33:04 +0800

    remove component dump dependences

--- tool/lib/copy.js
+++ tool/lib/copy.js
@@ -7,5 +7,12 @@ module.exports = function () {
   const antdProPkg = JSON.parse(fs.readFileSync(`${cwd}/config/components/package.json`, 'utf8'));
   antdProPkg.version = pkg.version;
   antdProPkg.dependencies = pkg.dependencies;
+  delete antdProPkg.dependencies.antd;
+  delete antdProPkg.dependencies.react;
+  delete antdProPkg.dependencies['react-dom'];
+  delete antdProPkg.dependencies['core-js'];
+  delete antdProPkg.dependencies.dva;
+  delete antdProPkg.dependencies.qs;
+  delete antdProPkg.dependencies['react-document-title'];
   fs.writeFileSync(`${cwd}/config/components/package.json`, JSON.stringify(antdProPkg, null, 2), 'utf8');
 };
