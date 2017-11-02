bae39e31d536d8bf0508ee2fbf12c1e0119373a8
nikogu <644506165@qq.com>
2017-11-02 13:36:40 +0800

    optimize tool code

--- tool/lib/copy.js
+++ tool/lib/copy.js
@@ -7,12 +7,20 @@ module.exports = function () {
   const antdProPkg = JSON.parse(fs.readFileSync(`${cwd}/config/components/package.json`, 'utf8'));
   antdProPkg.version = pkg.version;
   antdProPkg.dependencies = pkg.dependencies;
-  delete antdProPkg.dependencies.antd;
-  delete antdProPkg.dependencies.react;
-  delete antdProPkg.dependencies['react-dom'];
-  delete antdProPkg.dependencies['core-js'];
-  delete antdProPkg.dependencies.dva;
-  delete antdProPkg.dependencies.qs;
-  delete antdProPkg.dependencies['react-document-title'];
+
+  const dumps = [
+    'antd',
+    'react',
+    'react-dom',
+    'core-js',
+    'dva',
+    'qs',
+    'react-document-title',
+  ];
+
+  dumps.forEach((dump) => {
+    delete antdProPkg.dependencies[dump];
+  });
+
   fs.writeFileSync(`${cwd}/config/components/package.json`, JSON.stringify(antdProPkg, null, 2), 'utf8');
 };
