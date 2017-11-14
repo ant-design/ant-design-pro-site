2a0c05a2f91522ee180ec34faaa2f5377e4fea35
afc163 <afc163@gmail.com>
2017-11-14 16:11:12 +0800

    Updates

---
+++ docs/i18n.en-US.md.js
@@ -0,0 +1,1 @@
+webpackJsonp([47,59],{1544:function(e,n){e.exports={content:["article",["p","Planned for next version"]],meta:{order:12,title:"i18n",type:"Advanced",disabled:!0,filename:"docs/i18n.en-US.md"},toc:["ul"]}}});
--- docs/i18n.md.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([45,56],{1543:function(e,t){e.exports={content:["article",["p","\u5c06\u4f1a\u5728\u4e0b\u4e00\u4e2a\u7248\u672c\u5b9e\u73b0\u3002"]],meta:{order:12,title:{"en-US":"i18n","zh-CN":"\u56fd\u9645\u5316"},type:"\u8fdb\u9636",disabled:!0,filename:"docs/i18n.md"},toc:["ul"]}}});
