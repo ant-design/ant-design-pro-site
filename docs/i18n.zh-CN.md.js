58ff79a772dc3f7e7a66d6cc04f2cee849b95cc3
nikogu <644506165@qq.com>
2017-11-20 17:03:39 +0800

    Updates

--- docs/i18n.zh-CN.md.js
+++ docs/i18n.zh-CN.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([46,59],{1545:function(e,t){e.exports={content:["article",["p","\u5c06\u4f1a\u5728\u4e0b\u4e00\u4e2a\u7248\u672c\u5b9e\u73b0\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",disabled:!0,filename:"docs/i18n.zh-CN.md"},toc:["ul"]}}});
+webpackJsonp([46,59],{1560:function(e,t){e.exports={content:["article",["p","\u5c06\u4f1a\u5728\u4e0b\u4e00\u4e2a\u7248\u672c\u5b9e\u73b0\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",disabled:!0,filename:"docs/i18n.zh-CN.md"},toc:["ul"]}}});
