c826894cab010a16f61569266662b66f6bdd72d4
nikogu <644506165@qq.com>
2018-01-10 21:24:14 +0800

    Updates

--- docs/i18n.zh-CN.md.js
+++ docs/i18n.zh-CN.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([46,59],{1563:function(e,t){e.exports={content:["article",["p","\u5c06\u4f1a\u5728\u4e0b\u4e00\u4e2a\u7248\u672c\u5b9e\u73b0\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",disabled:!0,filename:"docs/i18n.zh-CN.md"},toc:["ul"]}}});
+webpackJsonp([50,66],{1689:function(e,t){e.exports={content:["article",["p","\u5c06\u4f1a\u5728\u4e0b\u4e00\u4e2a\u7248\u672c\u5b9e\u73b0\u3002"]],meta:{order:13,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",disabled:!0,filename:"docs/i18n.zh-CN.md"},toc:["ul"]}}});
