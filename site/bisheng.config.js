cfcafe4f1ed5a93534269110371e27293baaf7e8
jim <wasd2144@hotmail.com>
2018-04-08 20:13:42 +0800

    fix error https://github.com/ant-design/ant-design-pro/issues/1242

--- site/bisheng.config.js
+++ site/bisheng.config.js
@@ -1,19 +1,35 @@
-const path = require('path');
-const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
+const path = require("path");
+const replaceLib = require("antd-tools/lib/replaceLib");
+const isDev = process.env.NODE_ENV === "development";
 
-const isDev = process.env.NODE_ENV === 'development';
+function alertBabelConfig(rules) {
+  rules.forEach(rule => {
+    if (rule.loader && rule.loader === "babel-loader") {
+      if (rule.options.plugins.indexOf(replaceLib) === -1) {
+        rule.options.plugins.push(replaceLib);
+      }
+      rule.options.plugins = rule.options.plugins.filter(
+        plugin =>
+          !plugin.indexOf ||
+          plugin.indexOf("babel-plugin-add-module-exports") === -1
+      );
+    } else if (rule.use) {
+      alertBabelConfig(rule.use);
+    }
+  });
+}
 
 module.exports = {
   port: 8001,
-  root: '/',
+  root: "/",
   source: {
-    components: './scaffold/src/components',
-    docs: './docs',
+    components: "./scaffold/src/components",
+    docs: "./docs"
   },
-  theme: './site/theme',
-  htmlTemplate: './site/theme/static/template.html',
+  theme: "./site/theme",
+  htmlTemplate: "./site/theme/static/template.html",
   themeConfig: {
-    root: '/',
+    root: "/",
     isDev,
     typeOrder: {
       入门: 0,
@@ -21,34 +37,33 @@ module.exports = {
       其他: 2,
       Basic: 0,
       Advance: 1,
-      Other: 2,
-    },
+      Other: 2
+    }
   },
   filePathMapper(filePath) {
-    if (filePath === '/index.html') {
-      return ['/index.html', '/index-cn.html'];
+    if (filePath === "/index.html") {
+      return ["/index.html", "/index-cn.html"];
     }
-    if (filePath.endsWith('/index.html')) {
-      return [filePath, filePath.replace(/\/index\.html$/, '-cn/index.html')];
+    if (filePath.endsWith("/index.html")) {
+      return [filePath, filePath.replace(/\/index\.html$/, "-cn/index.html")];
     }
-    if (filePath !== '/404.html' && filePath !== '/index-cn.html') {
-      return [filePath, filePath.replace(/\.html$/, '-cn.html')];
+    if (filePath !== "/404.html" && filePath !== "/index-cn.html") {
+      return [filePath, filePath.replace(/\.html$/, "-cn.html")];
     }
     return filePath;
   },
   webpackConfig(config) {
     config.resolve.alias = {
-      'ant-design-pro/lib': path.join(process.cwd(), 'scaffold/src/components'),
-      'ant-design-pro': path.join(process.cwd(), 'config/components/index'),
-      site: path.join(process.cwd(), 'site'),
-      //'dva/router': 'react-router',
-      //'react-router': 'react-router/umd/ReactRouter',
+      "ant-design-pro/lib": path.join(process.cwd(), "scaffold/src/components"),
+      "ant-design-pro": path.join(process.cwd(), "config/components/index"),
+      site: path.join(process.cwd(), "site")
     };
+
     // components 下面的走 css module 其他不变
-    config.module.rules.forEach((loader) => {
+    config.module.rules.forEach(loader => {
       if (
-        typeof loader.test === 'function' &&
-        loader.test.toString().indexOf('\\.less$') > -1
+        typeof loader.test === "function" &&
+        loader.test.toString().indexOf("\\.less$") > -1
       ) {
         if (loader.exclude) {
           loader.exclude.push(/components/);
@@ -56,18 +71,19 @@ module.exports = {
           loader.exclude = [/components/];
         }
       }
-      if (loader.test.toString() === '/\\.module\\.less$/') {
+      if (loader.test.toString() === "/\\.module\\.less$/") {
         loader.test = /components.*.less$/;
       }
     });
-    if (isDev) {
-      config.devtool = 'source-map';
-    }
-    config.plugins.push(new CSSSplitWebpackPlugin({ size: 4000 }));
 
+    alertBabelConfig(config.module.rules);
+    
+    if (isDev) {
+      config.devtool = "source-map";
+    }
     return config;
   },
   htmlTemplateExtraData: {
-    isDev,
-  },
+    isDev
+  }
 };
