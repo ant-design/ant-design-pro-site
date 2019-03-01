c0974dc650c7870d71379017ea25213557cb9ecd
Kelvin Kamau <20049411+kelvinkamau@users.noreply.github.com>
2019-03-01 20:19:05 +0300

    Corrected grammatical error

--- site/theme/template/Home/Page1.jsx
+++ site/theme/template/Home/Page1.jsx
@@ -111,7 +111,7 @@ const featuresEN = [
   },
   {
     title: 'International',
-    content: 'Built-in i18n solution (Comming soon)',
+    content: 'Built-in i18n solution (Coming soon)',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/JsixxWSViARJnQbAAPkI.svg',
     color: '#722ED1',
     shadowColor: 'rgba(114,46,209,.12)',
