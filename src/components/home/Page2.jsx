45f6fd75584a53b3a3de35802c2bed57cbd41267
陈帅 <qixian.cs@outlook.com>
2019-05-08 18:00:39 +0800

    change git url to https

--- src/components/home/Page2.jsx
+++ src/components/home/Page2.jsx
@@ -25,7 +25,7 @@ function Page2({ location }) {
             </p>
             <div key="code1" className="home-code">
               <div>
-                $ <span>git clone</span> git@github.com:ant-design/ant-design-pro.git --depth=1
+                $ <span>git clone</span> https://github.com/ant-design/ant-design-pro.git --depth=1
               </div>
               <div>$ cd ant-design-pro</div>
               <div>$ npm install</div>
