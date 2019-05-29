6e9cd0076771cef3c94511b6c3f6ccf5babc5282
陈帅 <qixian.cs@outlook.com>
2019-05-29 18:40:54 +0800

    rm unuse code

--- gatsby/onCreateNode.js
+++ gatsby/onCreateNode.js
@@ -7,8 +7,9 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const himalaya = require('himalaya');
 
+// 获取用户的头像列表
 const getAvatarList = async filename => {
-  const sourcePath = 'https://github.com/ant-design/ant-design-pro-site/contributors/v4';
+  const sourcePath = 'https://github.com/ant-design/ant-design-pro-site/contributors/master';
   const url = `${sourcePath}${filename}/list`;
   const html = await fetch(url).then(res => res.text());
   const ast = himalaya.parse(html)[0].children || [];
