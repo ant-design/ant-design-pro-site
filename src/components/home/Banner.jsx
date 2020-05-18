f8fc53f9748b68a644cae4bd6378182a55ae9e3e
Scott Goci <scottjg@gmail.com>
2020-05-18 19:35:31 -0400

    Fix race condition on loading elements

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -12,49 +12,56 @@ function Banner(props) {
   const { isMobile, location } = props;
   return (
     <div className="banner-wrapper">
-      {isMobile && (
-        <TweenOne animation={{ opacity: 1 }} className="banner-image-wrapper">
-          <div className="home-banner-image">
-            <img
-              alt="banner"
-              src="https://gw.alipayobjects.com/zos/rmsportal/rqKQOpnMxeJKngVvulsF.svg"
-              width="100%"
+      <div className='banner-top-image'>
+        {isMobile && (
+          <TweenOne animation={{ opacity: 1, type: 'to' }} className="banner-image-wrapper banner-svg-img">
+            <div className="home-banner-image" key="home-banner-image">
+              <img
+                alt="banner"
+                src="https://gw.alipayobjects.com/zos/rmsportal/rqKQOpnMxeJKngVvulsF.svg"
+                width="100%"
+                key="banner-svg-img"
+              />
+            </div>
+          </TweenOne>
+        )}
+      </div>
+      <div className='banner-text'>
+        <QueueAnim className="banner-title-wrapper" type={isMobile ? 'bottom' : 'right'}>
+          <div key="line" className="title-line-wrapper">
+            <div className="title-line" style={{ transform: 'translateX(-64px)' }} />
+          </div>
+          <h1 key="h1">ANT DESIGN PRO</h1>
+          <p key="content">
+            <FormattedMessage id="app.home.slogan" />
+          </p>
+          <div key="button" className="button-wrapper">
+            <a href="http://preview.pro.ant.design" target="_blank" rel="noopener noreferrer">
+              <Button type="primary">
+                <FormattedMessage id="app.home.preview" />
+              </Button>
+            </a>
+            <Link to={getLocalizedPathname('/docs/getting-started', isZhCN(location.pathname))}>
+              <Button style={{ margin: '0 16px' }} type="primary" ghost>
+                <FormattedMessage id="app.home.start" />
+              </Button>
+            </Link>
+            <GitHubButton
+              key="github-button"
+              type="stargazers"
+              namespace="ant-design"
+              repo="ant-design-pro"
             />
           </div>
-        </TweenOne>
-      )}
-      <QueueAnim className="banner-title-wrapper" type={isMobile ? 'bottom' : 'right'}>
-        <div key="line" className="title-line-wrapper">
-          <div className="title-line" style={{ transform: 'translateX(-64px)' }} />
-        </div>
-        <h1 key="h1">ANT DESIGN PRO</h1>
-        <p key="content">
-          <FormattedMessage id="app.home.slogan" />
-        </p>
-        <div key="button" className="button-wrapper">
-          <a href="http://preview.pro.ant.design" target="_blank" rel="noopener noreferrer">
-            <Button type="primary">
-              <FormattedMessage id="app.home.preview" />
-            </Button>
-          </a>
-          <Link to={getLocalizedPathname('/docs/getting-started', isZhCN(location.pathname))}>
-            <Button style={{ margin: '0 16px' }} type="primary" ghost>
-              <FormattedMessage id="app.home.start" />
-            </Button>
-          </Link>
-          <GitHubButton
-            key="github-button"
-            type="stargazers"
-            namespace="ant-design"
-            repo="ant-design-pro"
-          />
-        </div>
-      </QueueAnim>
-      {!isMobile && (
-        <TweenOne animation={{ opacity: 1 }} className="banner-image-wrapper">
-          <BannerSVGAnim />
-        </TweenOne>
-      )}
+        </QueueAnim>
+      </div>
+      <div className='banner-bottom-img'>
+        {!isMobile && (
+          <TweenOne animation={{ opacity: 1, type: 'to' }} className="banner-image-wrapper banner-svg-anim" key='banner-bottom-img'>
+            <BannerSVGAnim key="banner-svg" />
+          </TweenOne>
+        )}
+      </div>
     </div>
   );
 }
