c85d57bdefc4101c1421984471c366aaa4bcd1a3
chenshuai2144 <qixian.cs@outlook.com>
2020-11-05 19:30:27 +0800

    fix cname path error

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,12 +4,3 @@ const fsExtra = require('fs-extra');
 exports.createPages = require('./gatsby/createPages');
 exports.onCreateNode = require('./gatsby/onCreateNode');
 exports.onCreatePage = require('./gatsby/onCreatePage');
-
-exports.onPostBuild = () => {
-  // 复制 CNAME
-  fsExtra.copyFileSync(`${__dirname}/CNAME`, `${__dirname}/public/CNAME`, (err) => {
-    if (err) {
-      console.error('Error copying file', err);
-    }
-  });
-};
