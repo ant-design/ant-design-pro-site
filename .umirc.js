639567207ac0d67a867039e0c7118ed83ce95276
dingtianxiu.dtx <dingtianxiu.dtx@antfin.com>
2021-06-30 11:59:09 +0800

    删除无用代码

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const menus = require('./formatMenu');
-console.log(menus);
 export default {
   title: 'Ant Design Pro',
   mode: 'site',
